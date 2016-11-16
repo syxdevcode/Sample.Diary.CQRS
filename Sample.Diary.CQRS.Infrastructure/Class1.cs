54269fec2e7d16bd838667b0abe3a8cec354c5d7
syxtest01@gmail.com <xing_*666888>
2016-11-16 15:16:28 +0800

    创建项目

---
+++ Sample.Diary.CQRS.Infrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Diary.CQRS.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sample.Diary.CQRS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sample.Diary.CQRS.Web.Startup))]
+namespace Sample.Diary.CQRS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
