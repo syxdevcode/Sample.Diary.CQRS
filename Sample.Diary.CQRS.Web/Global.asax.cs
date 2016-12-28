91d193ed7e7afd2363714849d601c12eae0b0e28
syxtest01@gmail.com <xing_*666888>
2016-12-28 17:37:36 +0800

    删除默认代码

--- Sample.Diary.CQRS.Web/Global.asax.cs
+++ Sample.Diary.CQRS.Web/Global.asax.cs
@@ -10,7 +10,6 @@ namespace Sample.Diary.CQRS.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            Bootstrapper.BootstrapStructureMap();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
